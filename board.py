495158ff08f16049e0cc46f667bfa354ce6e1546
aratakajitani <arata_kajitani@nuco.co.jp>
2026-07-13 18:23:36 +0900

    ひっくり返すように変更

--- board.py
+++ board.py
@@ -80,6 +80,96 @@ class Board:
 
         return False
 
+    def place_black(self, x, y):
+        self.field[y][x] = self.black
+
+    def place_white(self, x, y):
+        self.field[y][x] = self.white
+
+    def can_reverse_black(self, x, y):
+
+        if self.field[y][x] != self.empty:
+            return []
+
+        self.place_black(x, y)
+
+        all_reversible_stones = []
+
+        for dx, dy in self. direction:
+            next_x = x + dx
+            next_y = y + dy
+            opposite = False
+
+            while 0 <= next_x < self.size and 0 <= next_y < self.size:
+                if self.field[next_y][next_x] == self.empty:
+                    break
+                elif self.field[next_y][next_x] == self.white:
+                    opposite = True
+                elif self.field[next_y][next_x] == self.black:
+                    if opposite:
+                        can_reverse_stone = []
+                        while (next_y, next_x) != (y, x):
+                            next_x = next_x - dx
+                            next_y = next_y - dy
+                            if (next_y, next_x) == (y, x):
+                                break
+                            can_reverse_stone.append((next_y, next_x))
+                            print(can_reverse_stone)
+                            all_reversible_stones.extend(can_reverse_stone)
+                    break
+
+                next_x += dx
+                next_y += dy
+
+        return all_reversible_stones
+
+    def can_reverse_white(self, x, y):
+
+        if self.field[y][x] != self.empty:
+            return []
+
+        self.place_white(x, y)
+
+        all_reversible_stones = []
+
+        for dx, dy in self. direction:
+            next_x = x + dx
+            next_y = y + dy
+            opposite = False
+
+            while 0 <= next_x < self.size and 0 <= next_y < self.size:
+                if self.field[next_y][next_x] == self.empty:
+                    break
+                elif self.field[next_y][next_x] == self.black:
+                    opposite = True
+                elif self.field[next_y][next_x] == self.white:
+                    if opposite:
+                        can_reverse_stone = []
+                        while (next_y, next_x) != (y, x):
+                            next_x = next_x - dx
+                            next_y = next_y - dy
+                            if (next_y, next_x) == (y, x):
+                                break
+                            can_reverse_stone.append((next_y, next_x))
+                            print(can_reverse_stone)
+                            all_reversible_stones.extend(can_reverse_stone)
+                    break
+
+                next_x += dx
+                next_y += dy
+
+        return all_reversible_stones
+
+    def reverese_black(self, x, y):
+        can_reverse_stone = self.can_reverse_black(x, y)
+        for y, x in can_reverse_stone:
+            self.field[y][x] = self.black
+
+    def reverese_white(self, x, y):
+        can_reverse_stone = self.can_reverse_white(x, y)
+        for y, x in can_reverse_stone:
+            self.field[y][x] = self.white
+
     def show(self):
         for y in range(self.size):
             for x in range(self.size):
@@ -91,3 +181,4 @@ if __name__ == "__main__":
     board = Board()
     board.show()
     board.can_place_black()
+    board.place_black[2][3]
--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from board import Board
 def main():
     board = Board()
     board.show()
-    for y in range(board.size):
-        for x in range(board.size):
-            if board.can_place_black(x, y):
-                print(f"座標: ({x},{y})")
+    board.reverese_black(4, 6)
+    board.show()
 
 
 if __name__ == "__main__":
